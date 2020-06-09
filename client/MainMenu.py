04f3962b00b0fe93e33b029df806553ee9a36813
harleycool <apw18b@my.fsu.edu>
2020-06-09 12:38:20 -0400

    Deleted GUI folder and renamed GUIsetup to MainMenu

---
+++ client/MainMenu.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter.ttk import *
+
+def createMainMenu():
+    window = tk.Tk("test")
+    testLabel = tk.Label(text="Test label")
+    testLabel.pack()
--- client/client.py
+++ client/client.py
@@ -1,1 +1,6 @@
+from .GUI import MainMenu as menu
 
+def SetupClient():
+    menu.CreateMainMenu()
+
+SetupClient()
