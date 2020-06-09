bab64642ce77a1e0a3763ee9475f43077ee38257
harleycool <apw18b@my.fsu.edu>
2020-06-09 13:35:12 -0400

    Update MainMenu.py

--- client/GUI/MainMenu.py
+++ client/GUI/MainMenu.py
@@ -4,8 +4,14 @@ from tkinter.ttk import *
 def createMainMenu():
     window = tk.Tk()
     window.geometry("700x500")
-    testLabel = tk.Label(text="test")
-    testLabel.pack()
+
+    startButton = tk.Button(text="Start")
+    optionsButton = tk.Button(text="Options")
+    exitButton = tk.Button(text="Exit")
+    startButton.place(relheight=0.1, relwidth=0.2, relx=0.5, rely=0.5, y=-100, anchor = "center")
+    optionsButton.place(relheight=0.1, relwidth=0.2, relx=0.5, rely=0.5, anchor="center")
+    exitButton.place(relheight=0.1, relwidth=0.2, relx=0.5, rely=0.5, y=100, anchor="center")
+    
     window.mainloop()
 
 __package__ = "GUI"
