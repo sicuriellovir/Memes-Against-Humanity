ce84203acaa54383a1f3e4354b6431332336196d
sicuriellovir <54972072+sicuriellovir@users.noreply.github.com>
2020-07-29 21:44:14 -0400

    Update client.py

--- client/client.py
+++ client/client.py
@@ -2,6 +2,7 @@ import socket
 import select
 import errno
 import sys
+from .Card import PhraseCard, MemeCard
 
 def Connect(my_username):
 	HEADER_LENGTH = 10
@@ -9,41 +10,26 @@ def Connect(my_username):
 	IP = "127.0.0.1"
 	PORT = 1234
 
-	#my_username = input("Username: ")
 	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client_socket.connect((IP,PORT))
-	client_socket.setblocking(False)
-
 	username = my_username.encode('utf-8')
 	username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
 	client_socket.send(username_header + username)
 
 	while True:
-
-		message = input(f'{my_username} > ') #might need to change when gui is fully implemented
-
-		if message:
-
-			message = message.encode('utf-8')
-			message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-			client_socket.send(message_header+message)
-
 		try:
 			while True:
-				#receive things
-				username_header = client_socket.recv(HEADER_LENGTH)
-				if not len(username_header):
+				meme_header = client_socket.recv(1024)
+				if not len(meme_header):
 					print("Connection closed by the server")
-					sys.exit()
-
-				username_length = int(username_header.decode('utf-8').strip())
-				username = client_socket.recv(username_length).decode('utf-8')
-
-				message_header = client_socket.recv(HEADER_LENGTH)
-				message_length = int(message_header.decode('utf-8').strip())
-				message = client_socket.recv(message_length).decode('utf-8')
-
-				print(f'{username} > {message}')
+					sys.exit()				
+				meme_length = int(meme_header.decode('utf-8').strip())
+				meme = client_socket.recv(meme_length).decode('utf-8')
+				phrase_header = client_socket.recv(HEADER_LENGTH)
+				phrase_length = int(phrase_header.decode('utf-8').strip())
+				phrase = client_socket.recv(phrase_length).decode('utf-8')		
+				pCard = server.PhraseCard(phrase, my_username)
+				mCard = server.MemeCard(meme)
 
 		except IOError as e:
 			if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
